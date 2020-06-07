8e032a1852f85e7338d97c66077f3dda6b682465
Will_Zhu <will_zyq@qq.com>
2020-06-07 14:07:39 +1000

    update

--- table_widget_exercise.py
+++ table_widget_exercise.py
@@ -1,7 +1,7 @@
 import sys
 
-from PyQt5.QtWidgets import (QApplication, QWidget, QTableWidget, QVBoxLayout,\
-    QTableWidgetItem)
+from PyQt5.QtWidgets import (QApplication, QWidget, QTableWidget, QVBoxLayout,
+                             QTableWidgetItem, QHeaderView)
 
 
 class BaseModel(QWidget):
@@ -10,15 +10,46 @@ class BaseModel(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        table = QTableWidget(3, 3)
-        table.setHorizontalHeaderLabels(['Name', 'Model Type', 'Date'])
+        self.table = QTableWidget(3, 3)
+        # self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(['Name', 'Model Type', 'Date'])
         first_line = ('honese', 'Model1D', '2020-04-15')
         for i in range(0, 3):
             table_item = QTableWidgetItem(first_line[i])
-            table.setItem(0, i, table_item)
+            self.table.setItem(0, i, table_item)
 
+        # table内容不可编辑
+        self.table.setEditTriggers(QTableWidget.NoEditTriggers)
+
+        # table选中高亮一整行
+        self.table.setSelectionBehavior(QTableWidget.SelectRows)
+
+        # table仅能选中一个目标
+        self.table.setSelectionMode(QTableWidget.SingleSelection)
+
+        # table不显示网格
+        self.table.setShowGrid(False)
+
+        # table表头平铺并动态扩展
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # self.table.horizontalHeader().setHighlightSections(True)
+        self.table.setCornerButtonEnabled(False)
+        self.table.horizontalHeader().sectionDoubleClicked.connect(
+            lambda: print('header clicked'))
+        self.table.horizontalHeader().setSectionsClickable(False)
+        # self.table.verticalHeader().setHighlightSections(True)
+        self.table.verticalHeader().doubleClicked.connect(lambda: print(
+            "vertical header clicked"))
+        self.table.doubleClicked.connect(lambda: print('clicked'))
+        self.table.horizontalHeader().doubleClicked.connect(lambda: print(
+            'horizontal header clicked'))
+        self.table.itemDoubleClicked.connect(lambda: print("item clicked"))
+
+        self.table.cellDoubleClicked.connect(lambda: print("cell clicked"))
         v_box = QVBoxLayout()
-        v_box.addWidget(table)
+        v_box.addWidget(self.table)
         self.setLayout(v_box)
 
 
